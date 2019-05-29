5754024751128c4c99b45858a25a0da75a7be53b
Phil Girard <phil@redscientist.com>
2019-05-29 16:20:09 -0400

    Code Cleanup

--- Deploy/Objects.cs
+++ Deploy/Objects.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Deploy
+namespace RTCV.Deploy
 {
-    class BuildParams
+    public class BuildParams
     {
         public string productName = null;
         public string initials = null;
@@ -14,5 +15,49 @@ namespace Deploy
         public string pathFromProjectsToBuild = null;
         public string[] BuildDirDeleteDirectories = new string[0];
         public bool dupeDllCleanup = true;
+    }
+
+    public static class Deploy_Extensions
+    {
+        public static void DirectoryCopy(DirectoryInfo sourceDir, DirectoryInfo destDir, bool copySubDirs = true)
+        {
+            string sourceDirName = sourceDir.FullName;
+            string destDirName = destDir.FullName;
+            // Get the subdirectories for the specified directory.
+            DirectoryInfo dir = new DirectoryInfo(sourceDirName);
+
+            if (!dir.Exists)
+            {
+                throw new DirectoryNotFoundException(
+                    "Source directory does not exist or could not be found: "
+                    + sourceDirName);
+            }
+
+            DirectoryInfo[] dirs = dir.GetDirectories();
+            // If the destination directory doesn't exist, create it.
+            if (!Directory.Exists(destDirName))
+            {
+                Directory.CreateDirectory(destDirName);
+            }
+
+            // Get the files in the directory and copy them to the new location.
+            FileInfo[] files = dir.GetFiles();
+            foreach (FileInfo file in files)
+            {
+                string temppath = Path.Combine(destDirName, file.Name);
+                file.CopyTo(temppath, true);
+            }
+
+            // If copying subdirectories, copy them and their contents to new location.
+            if (copySubDirs)
+            {
+                foreach (DirectoryInfo subdir in dirs)
+                {
+                    string temppath = Path.Combine(destDirName, subdir.Name);
+                    DirectoryCopy(subdir, new DirectoryInfo(temppath), copySubDirs);
+                }
+            }
+        }
+
     }
 }
